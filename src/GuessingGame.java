c8c263ca3e90e6fa7276f8a65e9ed30677eb1dec
ericadavis <ericadavis@172.16.21.6>
2017-09-26 11:12:27 -0400

    This takes the guess and does everything.

---
+++ src/GuessingGame.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int userNum;
+		int computerNum = (int) (Math.random()*100) +1;
+		do {
+			String input = JOptionPane.showInputDialog(null, "Enter a guess between 0 and 100. Enter -1 to be a quitter :(");
+			userNum= Integer.parseInt(input);
+		}while (userNum != computerNum && userNum != -1);
+		if(userNum== computerNum) {
+			JOptionPane.showMessageDialog(null, "You guessed it! The number was:" + computerNum);
+		}
+		else if(userNum == -1) {
+			JOptionPane.showMessageDialog(null, "You are such a quitter");
+		}
+	}
+
+}
