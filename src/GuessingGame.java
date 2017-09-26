c82b3db89bd8b6c520ffec8b7c60c8b43f1be19d
ericadavis <ericadavis@172.16.21.6>
2017-09-26 11:18:49 -0400

    This takes input and prints out everything.

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -9,7 +9,10 @@ public class GuessingGame {
 		do {
 			String input = JOptionPane.showInputDialog(null, "Enter a guess between 0 and 100. Enter -1 to be a quitter :(");
 			userNum= Integer.parseInt(input);
-		}while (userNum != computerNum && userNum != -1);
+		}while (userNum != computerNum && userNum != -1 && userNum<=100 && userNum>=0);
+		if(userNum>100 || userNum<-1) {
+			JOptionPane.showMessageDialog(null, "can you follow simple directions?");
+		}
 		if(userNum== computerNum) {
 			JOptionPane.showMessageDialog(null, "You guessed it! The number was:" + computerNum);
 		}
