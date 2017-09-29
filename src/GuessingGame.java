74e01a6513af9eba51801b4eaf1abfca2d6afe5f
ericadavis <ericadavis@172.16.21.6>
2017-09-29 11:43:43 -0400

    This is the finished code!

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -5,16 +5,28 @@ public class GuessingGame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int userNum;
+		int score=0;
 		int computerNum = (int) (Math.random()*100) +1;
+		JOptionPane.showMessageDialog(null, computerNum);
 		do {
 			String input = JOptionPane.showInputDialog(null, "Enter a guess between 0 and 100. Enter -1 to be a quitter :(");
 			userNum= Integer.parseInt(input);
+			score+=1;
+			if (userNum >= computerNum -10 && userNum<= computerNum+10 && userNum != computerNum) {
+				JOptionPane.showMessageDialog(null,"You are within 10 points" );
+			}
+			else if(userNum >computerNum+10) {
+				JOptionPane.showMessageDialog(null, "Your guess was too high");
+			}
+			else if(userNum < computerNum){
+				JOptionPane.showMessageDialog(null, "your guess was too low!");
+			}
 		}while (userNum != computerNum && userNum != -1 && userNum<=100 && userNum>=0);
 		if(userNum>100 || userNum<-1) {
 			JOptionPane.showMessageDialog(null, "can you follow simple directions?");
 		}
 		if(userNum== computerNum) {
-			JOptionPane.showMessageDialog(null, "You guessed it! The number was:" + computerNum);
+			JOptionPane.showMessageDialog(null, "You guessed it! The number was:" + computerNum + ". You took " + score +" guess/es");
 		}
 		else if(userNum == -1) {
 			JOptionPane.showMessageDialog(null, "You are such a quitter");
